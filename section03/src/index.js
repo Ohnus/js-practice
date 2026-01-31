048a71e229c3320f69ae03dc346c1b3ecc9e10b1
SUNHO <sh525293@gmail.com>
2026-02-01 00:06:58 +0900

    Add sec03 - Module System

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,10 @@
+import mul, { add, sub } from "./math.js";
+
+// Common JS 모듈
+// const { add, sub } = require("./math");
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(3, 2));
+
 console.log("안녕 Node.js");
---
+++ section03/src/math.js
@@ -0,0 +1,13 @@
+// math 모듈
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// 기본 값으로 내보낸 모듈
+export default function multifly(a, b) {
+  return a * b;
+}
