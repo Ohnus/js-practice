54c53bd921a6626a9b2b16d4fdcfedebcf8f2a4f
SUNHO <sh525293@gmail.com>
2026-02-01 00:26:36 +0900

    Add sec03 - Library

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,7 +1,12 @@
+// 내부 모듈 호출
 import mul, { add, sub } from "./math.js";
-
-// Common JS 모듈
+// 외부 라이브러리 설치 및 모듈 호출
+import randomColor from "randomcolor";
+// Common JS 모듈 호출 방식
 // const { add, sub } = require("./math");
+
+const color = randomColor();
+console.log(color);
 
 console.log(add(1, 2));
 console.log(sub(1, 2));
